6a295595742995e7099512acdfc9e51e3daa3103
Dario Cesar Atach <datach@ar.ibm.com>
2024-09-15 09:47:11 -0300

    update

--- ecomerce59250/src/firebase/database.js
+++
@@ -1,3 +0,0 @@
-export const URL_FIREBASE = ('https://ecomerce-2d721-default-rtdb.firebaseio.com/')
-export const API_AUTH = 'AIzaSyD71et0zjukQY2Gyiqz3zgeq8Rhhmj5NtA'
-export const URL_AUTH = "https://identitytoolkit.googleapis.com/v1/"
